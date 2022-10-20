0e316f7288cf5412b63dd7f9cd160f699c0a65e0
Ian <ianwachira612@outlook.com>
2022-10-20 13:21:57 -0400

    nav.php

---
+++ nav.php
@@ -0,0 +1,8 @@
+<ul>
+    <nav>
+        <a href="index.php">Home</a> |
+        <a href="recipe-post.php">Post Your Recipes</a> |
+        <a href="recipe-list.php">Recipe List</a> 
+        <link rel="stylesheet" href="styles.css">  
+    </nav>
+</ul>
