3ae9a6179f91e4670af32c6c62b65d3abc27e99d
Ianw <ianwachira612@outlook.com>
2022-10-21 16:19:06 -0400

    recipe list page

---
+++ recipe-list.php
@@ -0,0 +1,17 @@
+<?php
+    include 'header.php';
+    include 'nav.php';
+?>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <main>
+
+  </main>
+  <?php
+    include 'footer.php';
+?>
+ </body>
+</html>
